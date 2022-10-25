b2b45780aca669f3d157b1811a12481f32be9df3
Pavels Ivanovs <32202454+pavelsivanovs@users.noreply.github.com>
2022-10-25 21:46:56 +0000

    Implemented aa function.

--- md2-1.hs
+++ md2-1.hs
@@ -1,5 +1,23 @@
-aa :: [] -> [] -> []
-aa args dict = []
-aa _ _ = _
+-- aa :: Eq a => [a] -> [(a, a)] -> [a]
+aa [] _ = []
+aa (a:as) dict
+  | snd el /= "" = snd el : aa as (removeElement el dict)
+  | otherwise = aa as dict
+  where el = dictElement a dict
 
-dict = [("I", "я"), ("eat", "їм"), ("ice cream", "морозиво"), ("bread", "хліб"), ("and", "та"), ("apple", "яблуко"), ("buy", "купую"), ("see", "бачу")]
+-- funkcija meklē vārdnīcā a elementam atbilstošu "tulkojumu"
+-- dictElement :: Eq a => a ->[(a, a)] -> (a, a)
+dictElement a (d:dicts)
+  | a == fst d = d
+  | otherwise = dictElement a dicts
+dictElement _ [] = ("none", "")
+
+-- funkcija atgriež jaunu vārdnīcu bez elementa el
+-- removeElement :: Eq a => a -> [a] -> [a]
+removeElement el (d:dicts)
+  | el == d = dicts
+  | otherwise = d : removeElement el dicts
+remove _ [] = []
+
+dict = [("I", "ya"), ("eat", "yim"), ("ice cream", "morozyvo"), ("bread", "khlib"), ("and", "ta"), ("apple", "yabluko"), ("buy", "kupuyu"), ("see", "bachu")]
+test_input = ["I", "eat", "apple", "and", "buy", "bread", "and", "ice cream"]
