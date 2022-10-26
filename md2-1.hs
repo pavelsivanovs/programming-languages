ddd7eb5d52fcc9b52bfd9b3f52a6f61ecff660ed
Pavels Ivanovs <32202454+pavelsivanovs@users.noreply.github.com>
2022-10-26 11:26:47 +0000

    Fixed removeElement function, so that it removes all duplicates from the array.

--- md2-1.hs
+++ md2-1.hs
@@ -1,4 +1,6 @@
--- aa :: Eq a => [a] -> [(a, a)] -> [a]
+-- import System.IO
+
+-- aa :: (Eq a) => [a] -> [(a, a)] -> [a]
 aa [] _ = []
 aa (a:as) dict
   | snd el /= "" = snd el : aa as (removeElement el dict)
@@ -13,11 +15,16 @@ dictElement a (d:dicts)
 dictElement _ [] = ("none", "")
 
 -- funkcija atgriež jaunu vārdnīcu bez elementa el
--- removeElement :: Eq a => a -> [a] -> [a]
+-- removeElement :: (Eq b) => b -> [b] -> [b]
 removeElement el (d:dicts)
-  | el == d = dicts
+  | snd el == snd d = removeElement el dicts
   | otherwise = d : removeElement el dicts
-remove _ [] = []
+removeElement _ [] = []
 
 dict = [("I", "ya"), ("eat", "yim"), ("ice cream", "morozyvo"), ("bread", "khlib"), ("and", "ta"), ("apple", "yabluko"), ("buy", "kupuyu"), ("see", "bachu")]
 test_input = ["I", "eat", "apple", "and", "buy", "bread", "and", "ice cream"]
+
+main = do
+  let example_list = ["1","2", "1", "3"]
+  let example_dict = [("1","viens"), ("2","divi"), ("3","viens")]
+  print $ aa example_list example_dict
