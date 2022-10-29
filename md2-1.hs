a56b0c0a763f74d44fef74a0fc9eb20aa31234df
Pavels Ivanovs <32202454+pavelsivanovs@users.noreply.github.com>
2022-10-29 19:28:32 +0000

    Finished Task 1 of HW 2

--- md2-1.hs
+++ md2-1.hs
@@ -7,6 +7,9 @@ aa (a:as) dict
     el = findElement a dict
 aa [] _ = []
 
+{-
+Funkcija pārbauda, vai padotais elements ir atrodams kā viens no vārdnicas indeksiem.
+-}
 isElemInDict :: (Eq a) => a -> [(a,a)] -> Bool
 isElemInDict el (d:dict)
   | found = True
@@ -14,13 +17,17 @@ isElemInDict el (d:dict)
   where found = el == fst d
 isElemInDict _ [] = False
 
--- funkcija meklē vārdnīcā a elementam atbilstošu "tulkojumu"
+{-
+Funkcija meklē vārdnīcā a elementam atbilstošu "tulkojumu".
+-}
 findElement :: (Eq a) => a ->[(a,a)] -> (a,a)
 findElement a (d:dicts)
   | a == fst d = d
   | otherwise = findElement a dicts
 
--- funkcija atgriež jaunu vārdnīcu bez elementa el
+{-
+Funkcija atgriež jaunu vārdnīcu bez elementa el.
+-}
 removeElement :: (Eq a) => (a,a) -> [(a,a)] -> [(a,a)]
 removeElement el (d:dicts)
   | snd el == snd d = removeElement el dicts
@@ -30,36 +37,84 @@ removeElement _ [] = []
 bb :: (Eq a) => [(a, a)] -> [(a, a)] -> [(a, a)]
 bb dicA dicB = removeDuplicates $ bb_main dicA dicB
 
+{-
+Funkcija atgriež ar duplikātiem divu vārdnīcu kompozīciju.
+Jaunā vārdnīca tiek veidota, atrodot kompozīcijas vārdnīcu no A saraksta 1. elementa
+un B vārdnīcas un tai konkatinējot vārdnīcu kompozīciju no palikušas A vārdnīcas un B vārdnīcas.
+-}
 bb_main :: (Eq a) => [(a, a)] -> [(a, a)] -> [(a, a)]
-bb_main (dA:dicA) (dB:dicB)
-  | found = (fst dA, snd elB) : (bb_main (dA:dicA) dicB ++ bb_main dicA (dB:dicB))
-  | otherwise = bb_main dicA (dB:dicB)
-  where 
-    found = isElemInDict (snd dA) (dB:dicB)
-    elB = findElement (snd dA) (dB:dicB)
+bb_main (dA:dicA) (dB:dicB) = bb_matchEl dA (dB:dicB) ++ bb_main dicA (dB:dicB)
 bb_main [] _ = []
 bb_main _ [] = []
 
+{-
+Funkcija atgriež jaunu vārdnīcu, kas ir padotu elementa un vārdnīcas kompozīcija.
+Rekursīvs darbības princips.
+-}
+bb_matchEl :: (Eq a) => (a,a) -> [(a,a)] -> [(a,a)]
+bb_matchEl dA (dB:dicB)
+  | similar = (fst dA, snd dB) : bb_matchEl dA dicB
+  | otherwise = bb_matchEl dA dicB
+  where similar = snd dA == fst dB
+bb_matchEl _ [] = []
+
+cc :: (Eq a) => [(a,a)] -> (Int, [(a,a)])
+cc a = cc_main (1,a,a)
+
+{-
+Funkcija atgriež a vārdnīcas slēgumu, rekursīvi pārbaudot, vai vārdnīcas N+1-slēgums
+ir vienāds ar vārdnīcas N-slēgumu. Rekursīvs darbības princips.
+-}
+cc_main :: (Eq a) => (Int, [(a,a)], [(a,a)]) -> (Int, [(a,a)])
+cc_main (m,a,aBase)
+  | same = (m,a)
+  | otherwise = cc_main (m+1, aComposed, aBase)
+  where
+    aComposed = removeDuplicates $ a ++ bb a aBase
+    same = a == aComposed
+
+{-
+Funkcija izdzēš duplikātus no vārdnīcas rekursīvā veidā.
+-}
 removeDuplicates :: (Eq a) => [a] -> [a]
 removeDuplicates (d:dict) = d : (removeDuplicates $ filter (/=d) dict)
 removeDuplicates [] = []
 
-dict = [("I", "ya"), ("eat", "yim"), ("ice cream", "morozyvo"), ("bread", "khlib"), ("and", "ta"), ("apple", "yabluko"), ("buy", "kupuyu"), ("see", "bachu")]
-test_input = ["I", "eat", "apple", "and", "buy", "bread", "and", "ice cream"]
+
+aa1 :: IO ()
+aa1 = do
+  let x = ["I", "eat", "apple", "and", "buy", "bread", "and", "ice cream"]
+  let y = [("I", "ya"), ("eat", "yim"), ("ice cream", "morozyvo"), ("bread", "khlib"), ("and", "ta"), ("apple", "yabluko"), ("buy", "kupuyu"), ("see", "bachu")]
+  print $ aa x y
+
+aa2 :: IO ()
+aa2 = do
+  let x = [1,2,3,4,5,4,6,4]
+  let y = [(1,10),(2,20),(3,30),(4,41),(4,42),(5,50),(6,60)]
+  print $ aa x y
 
 bb1 :: IO ()
 bb1 = do
-  let x = [(1,2),(2,3),(3,4),(4,5),(5,6),(6,8)]
-  let y = [(2,22),(3,33),(4,44),(5,55),(6,66),(7,77)]
+  let x = [(1,2),(2,3),(3,4),(4,5),(5,6),(7,8),(8,6)]
+  let y = [(2,20),(3,30),(4,40),(5,50),(6,60),(7,70)]
   print $ bb x y
 
 bb2 :: IO ()
 bb2 = do
   let x = [("a","b"), ("a","c"), ("c","d"), ("e","f"), ("g","h")]
-  let y = [("b","a"), ("b","b"), ("b","c"), ("b","d"), ("b","a"), ("b","a"), ("c","d"), ("f","ff")]
+  let y = [("b","a"), ("b","b"), ("b","c"), ("b","d"), ("b","a"), ("b","a"), ("c","a"), ("c","d"), ("f","ff")]
   print $ bb x y
 
-main = do
-  let example_list = ["1","2", "1", "3"]
-  let example_dict = [("1","viens"), ("2","divi"), ("3","viens")]
-  print $ aa example_list example_dict
+cc1 :: IO ()
+cc1 = do
+  let x = [(1,2),(2,3),(3,4),(4,5),(5,1)]
+  let (slegums, px) = cc x
+  print slegums
+  print px
+
+cc2 :: IO ()
+cc2 = do
+  let x = [("a","b"),("b","c"),("c","d"),("d","e"),("c","e"),("e","a")]
+  let (slegums, px) = cc x
+  print slegums
+  print px
